7bea0f29cec229ba3a145ca4a79950a75af47664
Danh Tran Cong <trancongdanh99@gmail.com>
2025-05-25 00:17:08 +0700

    Add Open Letter Component

---
+++ components/open-letter.tsx
@@ -0,0 +1,23 @@
+"use client";
+import './open-letter.scss'
+
+export default function OpenLetter() {
+  return (
+    <div className="dy-open-letter">
+      <div className="header-1">Lời ngỏ</div>
+      <div className="content">
+        <div>Gửi những người thân yêu nhất của Công Danh và Ngô Yến!</div>
+        <div>
+          Thế là sau bao nhiêu ngày &quot;cùng nhau &quot;, cuối cùng chúng mình
+          cũng quyết định về chung một nhà! Và hôn nhân là chuyện trăm năm, là
+          ngày trọng nhất cuộc đời... vậy nên Danh - Yến vô cùng hạnh phúc khi
+          được mời tất cả những người mình yêu quý đến chung vui, cùng chứng
+          kiến và chia sẻ niềm hạnh phúc đặc biệt này.
+        </div>
+        <div>
+          Hãy cùng Danh - Yến viết nên những kỷ niệm đẹp đẽ và đáng nhớ nhé!
+        </div>
+      </div>
+    </div>
+  );
+}
