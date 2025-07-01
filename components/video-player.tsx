b0f9630857001b746b90c4fda8d69a276b2a22e5
Danh Tran Cong <trancongdanh99@gmail.com>
2025-07-01 23:45:27 +0700

    Hot fix

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,5 +1,56 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { FaVolumeHigh } from "react-icons/fa6";
 import "./video-player.scss";
 export default function VideoPlayer() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const toolTipRef = useRef<HTMLDivElement>(null);
+  const unmuteBtn = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const videoEl = videoRef.current;
+    if (!videoEl) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          videoEl.play().catch((err) => {
+            console.warn("Autoplay failed:", err);
+          });
+        } else {
+          videoEl.pause();
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    observer.observe(videoEl);
+
+    return () => {
+      observer.unobserve(videoEl);
+    };
+  }, []);
+
+  const handleUnmute = () => {
+    const video = videoRef.current;
+    if (video) {
+      video.muted = false;
+    }
+    if (!unmuteBtn.current) return;
+    hideToolTip();
+  };
+
+  const hideToolTip = () => {
+    const toolTip = toolTipRef.current;
+    if (!toolTip) return;
+    toolTip.style.display = "none";
+    if (!unmuteBtn.current) return;
+    unmuteBtn.current.style.display = "none";
+  };
+
   return (
     <div className="dy-video-player">
       <div className="dy-video-player__content">
@@ -12,13 +63,23 @@ export default function VideoPlayer() {
       </div>
       <div className="dy-video-player__video">
         <video
+          ref={videoRef}
           src="/video.mp4"
           preload="auto"
+          playsInline
+          muted
           controls
           height={"100%"}
           width={"100%"}
           poster="/thumbnail.jpg"
         ></video>
+        <button ref={unmuteBtn} className="dy-button" onClick={handleUnmute}>
+          <FaVolumeHigh color="#6b513c" />
+        </button>
+        <div className="tool-tip" ref={toolTipRef}>
+          <div>Nhấn vào icon để  bên dưới để bật âm thanh nhé!</div>
+          <button onClick={hideToolTip}>X</button>
+        </div>
       </div>
     </div>
   );
