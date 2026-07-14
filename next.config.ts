5646b1fdf5ee693d6dc972ebf7b52b6e0009317a
Cong-Danh Tran <trancongdanh99@gmail.com>
2026-07-14 12:25:51 +0700

    Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/dy-wedding-website" : "",
-  assetPrefix: isProd ? "/dy-wedding-website/" : "",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
